7737dd2885e09c4f27659a608380080d251a5483
Cass Bredeson <c.s.bredeson@gmail.com>
2021-01-14 00:57:48 +0000

    added vertical test and additional test cases

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const assert = chai.assert;
 
-const wordSearch = require('../wordsearch.js')
+const wordSearch = require('../wordsearch.js');
 
 describe("#wordSearch()", function() {
   it("should return false if the word is not present", function() {
@@ -15,12 +15,12 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'FRANK')
+    ], 'FRANK');
 
     assert.isFalse(result);
   });
 
-  it("should return true if the word is present", function() {
+  it("should return true if the word is present horizontally", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -31,8 +31,28 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'SEINFELD')
-
+    ], 'SEINFELD');
     assert.isTrue(result);
   });
+
+  it("should return true if the word is present vertically", function() {
+    const result = wordSearch([
+      ["a", "b", "o", "s", "t"],
+      ["k", "o", "o", "e", "r"],
+      ["b", "a", "g", "h", "p"],
+      ["s", "t", "i", "w", "b"]],
+    "boat");
+    assert.isTrue(result);
+  });
+
+  it("Should return false if an array is empty", function() {
+    const result = wordSearch([], "banana");
+    assert.isFalse(result);
+  });
+
+  it("Should return false if the arrays within the array are empty", function() {
+    const result = wordSearch([[], [], [], []], "potato");
+    assert.isFalse(result);
+  });
+
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,23 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+  if (letters.length === 0) return false;
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (const l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+  let array = [];
+  for (let i = 0; i < letters[0].length; i++) {
+    array.push([letters[0][i]]);
+  }
+  for (let row = 0; row < letters.length; row++) {
+    for (let column = 0; column < letters[row].length; column++) {
+      array[column][row] = letters[row][column];
     }
-}
+  }
+  const verticalJoin = array.map(ls => ls.join(''));
+  for (const row of verticalJoin) {
+    if (row.includes(word)) return true;
+  }
+  return false;
+};
 
-module.exports = wordSearch
+module.exports = wordSearch;
